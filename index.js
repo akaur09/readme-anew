f50970d2a402206a95e0572c5c9b7fae423e9d98
akaur091 <amniderkaur.09@gmail.com>
2022-04-20 23:24:39 -0700

    add prompt questions

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What kind of installation is required for your project to work?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the use of this project?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Who are the contributers for this project?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What tests were done?',
+    },
+    {
+        type: 'input',
+        name: 'questions1',
+        message: 'Add a link to your Github profile for questions.',
+    },
+    {
+        type: 'input',
+        name: 'question2',
+        message: 'Add your email address to be reached for questions?',
+    },
+    {
+        type: '',
+        name: 'license',
+        message: 'What kind of lincense would you like to add',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
